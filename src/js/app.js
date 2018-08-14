3e78b7a94508ec5c7ca28aeb9bd2856da78acf85
annavu <anna.wrzos@gmail.com>
2018-08-14 14:43:45 +0200

    change class name, update function

--- src/js/app.js
+++ src/js/app.js
@@ -9,26 +9,46 @@ import '../scss/main.scss';
 
 const lastRow = document.querySelector('.row--last');
 const table = document.querySelector('.table__content');
-
-lastRow.addEventListener('mouseenter', function() {
-  table.classList.add('add');
-})
-
-lastRow.addEventListener('mouseleave', function() {
-  table.classList.remove('add');
-})
-
 const rows = document.querySelectorAll('.row');
 const deleteButtons = document.querySelectorAll('.cell--img');
 const icons = document.querySelectorAll('.fa');
 
+
+function addClass() {
+  table.classList.add('table__content--height');
+}
+
+function removeClass() {
+  table.classList.remove('table__content--height');
+}
+
+lastRow.addEventListener('mouseenter', addClass)
+
+lastRow.addEventListener('mouseleave', removeClass)
+
+lastRow.addEventListener('click', addClass)
+
+
+
 deleteButtons.forEach(function(btn) {
   btn.addEventListener('click', function() {
-    btn.parentNode.classList.add('row--clicked');
-    btn.parentNode.classList.add('nohover');
-    console.log(btn.children[1])
-    btn.children[1].remove()
-    // btn.parentNode.remove();
-    // table.style.height = 'auto';
+
+    if(btn.parentNode.classList.contains('row--last')) {
+      console.log(123);
+      btn.parentNode.removeEventListener('mouseleave', removeClass)
+      btn.parentNode.classList.add('row--clicked');
+      btn.parentNode.classList.add('nohover');
+      console.log(btn.children[1])
+      btn.children[1].remove()
+      // btn.parentNode.remove();
+      // table.style.height = 'auto';
+    } else {
+        btn.parentNode.classList.add('row--clicked');
+        btn.parentNode.classList.add('nohover');
+        console.log(btn.children[1])
+        btn.children[1].remove()
+        // btn.parentNode.remove();
+        // table.style.height = 'auto';
+    }
   })
 })
